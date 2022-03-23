af116be79eb84d4ef9a2ddce99f80de17a6c90a7
Gastón Cuesta <gastoncuesta92@gmail.com>
2022-03-23 16:50:00 +0100

    First commit

---
+++ src/test/java/com/amazingco/companystructure/CompanyStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazingco.companystructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
