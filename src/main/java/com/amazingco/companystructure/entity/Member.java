e089b2d6da0ee60325b31189996857c9df2a3e15
Gastón Cuesta <gastoncuesta92@gmail.com>
2022-04-08 20:26:37 +0200

    I finish all the test

--- src/main/java/com/amazingco/companystructure/entity/Member.java
+++ src/main/java/com/amazingco/companystructure/entity/Member.java
@@ -12,6 +12,8 @@ public class Member {
 
     private String name;
     private Member parent;
+    private Member root;
+    private int height;
     private List<Member> children;
 
 
@@ -21,6 +23,8 @@ public class Member {
         }
         if (!existChild(member)) {
             member.setParent(this);
+            addRoot(member);
+            addHeight(member);
             children.add(member);
         }
     }
@@ -29,5 +33,78 @@ public class Member {
         return children.stream().anyMatch(child -> child.getName().equalsIgnoreCase(person.getName()));
     }
 
+
+    public static List<Member> getAllDescendants(Member member) {
+        List<Member> descendants = new ArrayList<>();
+        List<Member> children = member.getChildren();
+
+        while (children != null) {
+
+            for (Member child : children) {
+                descendants.addAll(children);
+                children = child.getChildren();
+            }
+
+        }
+
+        return descendants;
+    }
+
+
+    public void changeParent(Member newParent) {
+        newParent.addChild(this);
+        parent = newParent;
+    }
+
+
+    private static void addRoot(Member member) {
+        Member root = member.getParent();
+
+        while (root != null) {
+
+            if (root.getParent() == null) {
+                member.root = root;
+                break;
+
+            } else {
+                root = root.getParent();
+            }
+        }
+    }
+
+    private static void addHeight(Member member) {
+        member.height = 0;
+        Member root = member.getParent();
+        while (root != null) {
+
+            member.height = member.height + 1;
+
+            root = root.getParent();
+        }
+
+    }
+
+
+    private String getChildrenNames() {
+        StringBuilder names = new StringBuilder();
+        if (children != null) {
+            for (Member child : children) {
+                names.append(child.getName()).append(" - ");
+            }
+        }
+        return names.toString();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                ", parent=" + parent +
+                ", root=" + root +
+                ", height=" + height +
+                ", children=" + getChildrenNames() +
+                '}';
+    }
 
 }
--- src/test/java/com/amazingco/companystructure/CompanyStructureApplicationTests.java
+++ src/test/java/com/amazingco/companystructure/CompanyStructureApplicationTests.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class CompanyStructureApplicationTests {
@@ -24,43 +25,165 @@ class CompanyStructureApplicationTests {
     }
 
     @Test
-    @DisplayName("Add One child to the parent")
-    void testAddChildToRoot() {
-        Member child = Member.builder().name("a").parent(root).build();
-        root.addChild(child);
-
-        assertEquals(1, root.getChildren().size(), "It should have one child");
+    @DisplayName("Get first child of root")
+    void testGetFirstChild() {
+        Member firstChild = Member.builder().name("firstChild").parent(root).build();
+        assertEquals(firstChild.getName(), "firstChild", "Name of firstChild should be firstChild");
+        assertEquals(firstChild.getParent(), root, "Parent of firstChild should be root");
     }
 
     @Test
-    @DisplayName("Add Two Equal children to parent")
-    void testAddTwoEqualChildrenToFather() {
-        root.addChild(Member.builder().name("h").build());
-        root.addChild(Member.builder().name("h").build());
-        assertNotEquals(2, root.getChildren().size(), "It should have just one child");
+    @DisplayName("Get second child of root")
+    void testGetSecondChild() {
+        Member.builder().name("firstChild").parent(root).build();
+        Member secondChild = Member.builder().name("secondChild").parent(root).build();
+        assertEquals(secondChild.getName(), "secondChild", "Name of secondChild should be secondChild");
+        assertEquals(secondChild.getParent(), root, "Parent of secondChild should be root");
     }
-
 
     @Test
-    @DisplayName("Add children to parent")
-    void testAddManyChildrenToParent() {
-        Member childA = Member.builder().name("a").parent(root).build();
-        Member childB = Member.builder().name("b").parent(root).build();
-        Member childC = Member.builder().name("c").parent(root).build();
-        Member childD = Member.builder().name("d").parent(root).build();
-        Member childE = Member.builder().name("e").parent(root).build();
-
-        List<Member> children = new ArrayList<>() {{
-            add(childA);
-            add(childB);
-            add(childC);
-            add(childD);
-            add(childE);
-        }};
-
-        root.setChildren(children);
-
-        assertTrue(root.getChildren().size() > 1, "It should have many children");
+    @DisplayName("Get first child of first child")
+    void testGetFirstChildOfFirstChild() {
+        Member firstChild = Member.builder().name("firstChild").parent(root).build();
+        Member firstChildOfFirstChild = Member.builder().name("firstChildOfFirstChild").parent(firstChild).build();
+        assertEquals(firstChildOfFirstChild.getName(), "firstChildOfFirstChild", "Name of firstChildOfFirstChild should be firstChildOfFirstChild");
+        assertEquals(firstChildOfFirstChild.getParent(), firstChild, "Parent of firstChildOfFirstChild should be firstChild");
     }
 
+    @Test
+    @DisplayName("Get second child of first child")
+    void testGetSecondChildOfFirstChild() {
+        Member firstChild = Member.builder().name("firstChild").parent(root).build();
+        Member.builder().name("firstChildOfFirstChild").parent(firstChild).build();
+        Member secondChildOfFirstChild = Member.builder().name("secondChildOfFirstChild").parent(firstChild).build();
+        assertEquals(secondChildOfFirstChild.getName(), "secondChildOfFirstChild", "Name of secondChildOfFirstChild should be secondChildOfFirstChild");
+        assertEquals(secondChildOfFirstChild.getParent(), firstChild, "Parent of secondChildOfFirstChild should be firstChild");
+    }
+
+    @Test
+    @DisplayName("Get first child of second child")
+    void testGetFirstChildOfSecondChild() {
+        Member.builder().name("firstChild").parent(root).build();
+        Member secondChild = Member.builder().name("secondChild").parent(root).build();
+        Member firstChildOfSecondChild = Member.builder().name("firstChildOfSecondChild").parent(secondChild).build();
+        assertEquals(firstChildOfSecondChild.getName(), "firstChildOfSecondChild", "Name of firstChildOfSecondChild should be firstChildOfSecondChild");
+        assertEquals(firstChildOfSecondChild.getParent(), secondChild, "Parent of firstChildOfSecondChild should be secondChild");
+    }
+
+    @Test
+    @DisplayName("Get second child of second child")
+    void testGetSecondChildOfSecondChild() {
+        Member.builder().name("firstChild").parent(root).build();
+        Member secondChild = Member.builder().name("secondChild").parent(root).build();
+        Member secondChildOfSecondChild = Member.builder().name("secondChildOfSecondChild").parent(secondChild).build();
+        assertEquals(secondChildOfSecondChild.getName(), "secondChildOfSecondChild", "Name of secondChildOfSecondChild should be secondChildOfSecondChild");
+        assertEquals(secondChildOfSecondChild.getParent(), secondChild, "Parent of secondChildOfSecondChild should be secondChild");
+    }
+
+    @Test
+    @DisplayName("Get first child of second child of first child")
+    void testGetFirstChildOfSecondChildOfFirstChild() {
+        Member.builder().name("firstChild").parent(root).build();
+        Member secondChild = Member.builder().name("secondChild").parent(root).build();
+        Member firstChildOfSecondChild = Member.builder().name("firstChildOfSecondChild").parent(secondChild).build();
+        Member firstChildOfSecondChildOfFirstChild = Member.builder().name("firstChildOfSecondChildOfFirstChild").parent(firstChildOfSecondChild).build();
+        assertEquals(firstChildOfSecondChildOfFirstChild.getName(), "firstChildOfSecondChildOfFirstChild", "Name of firstChildOfSecondChildOfFirstChild should be firstChildOfSecondChildOfFirstChild");
+        assertEquals(firstChildOfSecondChildOfFirstChild.getParent(), firstChildOfSecondChild, "Parent of firstChildOfSecondChildOfFirstChild should be firstChildOfSecondChild");
+    }
+
+    @Test
+    @DisplayName("Get all descendants of root")
+    void testGetAllDescendantsOfRoot() {
+        Member firstChild = Member.builder().name("firstChild").build();
+        Member secondChild = Member.builder().name("secondChild").build();
+        root.addChild(firstChild);
+        root.addChild(secondChild);
+
+        Member firstChildOfFirstChild = Member.builder().name("firstChildOfFirstChild").build();
+        Member secondChildOfFirstChild = Member.builder().name("secondChildOfFirstChild").build();
+        firstChild.addChild(firstChildOfFirstChild);
+        firstChild.addChild(secondChildOfFirstChild);
+
+        Member firstChildOfSecondChild = Member.builder().name("firstChildOfSecondChild").build();
+        Member secondChildOfSecondChild = Member.builder().name("secondChildOfSecondChild").build();
+        secondChild.addChild(firstChildOfSecondChild);
+        secondChild.addChild(secondChildOfSecondChild);
+
+        Member firstChildOfSecondChildOfFirstChild = Member.builder().name("firstChildOfSecondChildOfFirstChild").build();
+        Member secondChildOfSecondChildOfFirstChild = Member.builder().name("secondChildOfSecondChildOfFirstChild").build();
+        firstChildOfSecondChild.addChild(firstChildOfSecondChildOfFirstChild);
+        firstChildOfSecondChild.addChild(secondChildOfSecondChildOfFirstChild);
+
+        List<Member> descendants = Arrays.asList(firstChild, secondChild, firstChildOfFirstChild, secondChildOfFirstChild, firstChildOfSecondChild, secondChildOfSecondChild, firstChildOfSecondChildOfFirstChild, secondChildOfSecondChildOfFirstChild);
+        assertEquals(Member.getAllDescendants(root), descendants, "All descendants of root should be firstChild, secondChild, firstChildOfFirstChild, secondChildOfFirstChild, firstChildOfSecondChild, secondChildOfSecondChild, firstChildOfSecondChildOfFirstChild, secondChildOfSecondChildOfFirstChild");
+    }
+
+    @Test
+    @DisplayName("Change the parent of a node")
+    void changeTheParentofANode() {
+        Member firstChild = Member.builder().name("firstChild").build();
+        Member secondChild = Member.builder().name("secondChild").build();
+        root.addChild(firstChild);
+        root.addChild(secondChild);
+
+        Member firstChildOfFirstChild = Member.builder().name("firstChildOfFirstChild").build();
+        Member secondChildOfFirstChild = Member.builder().name("secondChildOfFirstChild").build();
+        firstChild.addChild(firstChildOfFirstChild);
+        firstChild.addChild(secondChildOfFirstChild);
+
+        Member newParent = Member.builder().name("newParent").build();
+        root.addChild(newParent);
+        firstChildOfFirstChild.changeParent(newParent);
+
+        assertEquals("newParent", firstChildOfFirstChild.getParent().getName(), "firstChildOfFirstChild's parent's name should be newParent");
+        assertEquals("firstChildOfFirstChild", newParent.getChildren().get(0).getName(), "child of newParent should be firstChildOfFirstChild");
+    }
+
+    @Test
+    @DisplayName("Get the root of the node")
+    void getRootOfTheNode() {
+        Member firstChild = Member.builder().name("firstChild").build();
+        Member secondChild = Member.builder().name("secondChild").build();
+        root.addChild(firstChild);
+        root.addChild(secondChild);
+
+        Member firstChildOfFirstChild = Member.builder().name("firstChildOfFirstChild").build();
+        Member secondChildOfFirstChild = Member.builder().name("secondChildOfFirstChild").build();
+        firstChild.addChild(firstChildOfFirstChild);
+        firstChild.addChild(secondChildOfFirstChild);
+
+        Member firstChildOfSecondChild = Member.builder().name("firstChildOfSecondChild").build();
+        Member secondChildOfSecondChild = Member.builder().name("secondChildOfSecondChild").build();
+        secondChild.addChild(firstChildOfSecondChild);
+        secondChild.addChild(secondChildOfSecondChild);
+
+        Member firstChildOfFirstChildOfFirstChild = Member.builder().name("firstChildOfFirstChildOfFirstChild").build();
+        firstChildOfFirstChild.addChild(firstChildOfFirstChildOfFirstChild);
+
+        assertNull(root.getRoot(), "root should be null");
+        assertEquals(root, secondChild.getRoot(), "The root of the node should be root");
+        assertEquals(root, secondChildOfSecondChild.getRoot(), "The root of the node should be root");
+        assertEquals(root, firstChildOfFirstChildOfFirstChild.getRoot(), "The root of the node should be root");
+    }
+
+    @Test
+    void testSetNodeHeight(){
+
+        Member firstChild = Member.builder().name("firstChild").parent(root).build();
+        Member secondChild = Member.builder().name("secondChild").build();
+        root.addChild(firstChild);
+        root.addChild(secondChild);
+
+        Member firstChildOfFirstChild = Member.builder().name("firstChildOfFirstChild").build();
+        Member secondChildOfFirstChild = Member.builder().name("secondChildOfFirstChild").build();
+        firstChild.addChild(firstChildOfFirstChild);
+        firstChild.addChild(secondChildOfFirstChild);
+
+
+        assertEquals(0, root.getHeight(), "The height of root should be 0");
+        assertEquals(1, firstChild.getHeight(), "The height of the node should be 1");
+        assertEquals(1, secondChild.getHeight(), "The height of the node should be 1");
+        assertEquals(2, firstChildOfFirstChild.getHeight(), "The height of the node should be 2");
+
+    }
 }
